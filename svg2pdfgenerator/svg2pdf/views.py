b0fb511f268d42fc1697bb44d6cb48eef2a810c8
kacper <poziomy824@gmail.com>
2022-01-04 14:20:31 +0100

    faktura w sgv bez tabeli

--- svg2pdfgenerator/svg2pdf/views.py
+++ svg2pdfgenerator/svg2pdf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 class firma:
     def __init__(self, nazwa, nip, ulica, addres):
@@ -9,13 +10,31 @@ class firma:
         self.ulica = ulica
         self.addres = addres
 
+class usluga:
+    def __init__(self, nazwa, jm, ilosc, cenaN, wartoscN):
+        self.nazwa = nazwa
+        self.jm = jm
+        self.ilosc = ilosc
+        self.cenaN = cenaN
+        self.wartoscN = wartoscN
+        self.stawkaVat = 1.23
+        self.kwotaVat = round(cenaN * self.stawkaVat)
+        self.WartoscVat = round (wartoscN * self.stawkaVat)
+
+
 
 content = {
     "miejsceWystawienia": 'Wesoła',
     "dataWystawienia": "04 - 01 - 2022",
     "dataWykonaniaUslugi": "03 - 01 - 2022",
     'firmasprzedawcza': firma('firma a', '1234567890', 'xyz 10', '10-100 xyz'),
-    'firmanabywcza': firma('firma b', '0987654321', 'xyz 21', '11-111 xyz')
+    'firmanabywcza': firma('firma b', '0987654321', 'xyz 21', '11-111 xyz'),
+    "datafakturaVat": '02/02/2022',
+    'uslogi':[
+        usluga('sysop', 'usł.', 1, 21.23, 1),
+        usluga('sysop', 'usł.', 1, 21.23, 1),
+        usluga('sysop', 'usł.', 1, 21.23, 1),
+    ]
 }
 
 
