7676d5f21615517105f51d285a3c20baad4a5297
Kacper W <poziomy824@gmail.com>
2022-02-01 14:48:52 +0100

    opcja generowania faktury

--- svg2pdf/urls.py
+++ svg2pdf/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.strona_gl),
-    path('faktura-<int:id>/', views.faktura_temp)
+    path('faktura-<int:id>/', views.faktura_temp),
+    path('fakturag-<int:id>/', views.faktura_gen)
 ]
--- svg2pdf/views.py
+++ svg2pdf/views.py
@@ -1,3 +1,5 @@
+from logging import exception
+import re
 from django.http.response import FileResponse, HttpResponse
 from django.shortcuts import render
 from .models import *
@@ -8,7 +10,21 @@ def strona_gl(request):
     return render(request, 'strona_gl.html', {"faktura_ostatnia" : faktury})
 
 def faktura_temp(request, id=1):
+    #get faktura by id
+    faktury = faktura.objects.order_by('-id')
+    for x in faktury:
+        if x.id == id:
+            id = x
+    try:
+        return FileResponse(open(f'faktura/{x.Nazwa_faktury}.pdf', 'rb'), as_attachment=0, filename=f'{x.Nazwa_faktury}.pdf')
+    except:
+        context = getcontext(id)
+        context, pozycje_c, tabelarys = faktura_context_calc(context)
+        context_to_pdf(context, pozycje_c, tabelarys, x.Nazwa_faktury)
 
+        return FileResponse(open(f'faktura/{x.Nazwa_faktury}.pdf', 'rb'), as_attachment=0, filename=f'{x.Nazwa_faktury}.pdf')
+
+def faktura_gen(request, id=1):
     #get faktura by id
     faktury = faktura.objects.order_by('-id')
     for x in faktury:
@@ -18,7 +34,6 @@ def faktura_temp(request, id=1):
     #calc context
     context = getcontext(id)
     context, pozycje_c, tabelarys = faktura_context_calc(context)
-    context_to_pdf(context, pozycje_c, tabelarys, 'faktura')
+    context_to_pdf(context, pozycje_c, tabelarys, x.Nazwa_faktury)
 
-    return FileResponse(open('faktura/faktura.pdf', 'rb'), as_attachment=0, filename='faktura.pdf')
-
+    return FileResponse(open(f'faktura/{x.Nazwa_faktury}.pdf', 'rb'), as_attachment=0, filename=f'{x.Nazwa_faktury}.pdf')
