10b92046218d9dfaa5edccabf41e7c762e6693bf
Kacper W <poziomy824@gmail.com>
2022-02-01 13:49:23 +0100

    trzy nowe testy

--- tests.py
+++ tests.py
@@ -1,4 +1,3 @@
-from logging import exception
 import pytest
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
@@ -38,10 +37,47 @@ def basic_con():
 try:
     shutil.rmtree('faktura_testy')
 except:
-    print(exception)
+    pass
 #tests
 def test_podstawowy():
     context = basic_con()
     context, pozycje_c, tabelarys = faktura_context_calc(context)
     context_to_pdf(context, pozycje_c, tabelarys, 'test_podstawowy', 'faktura_testy')
     assert os.path.exists('faktura_testy/test_podstawowy.pdf')
+def test_pozycje():
+    context = basic_con()
+    temp = []
+    for x in range(100):
+        temp += [pozycja(f'poz {x}', x)]
+    context.update({
+        'POZYCJE': temp
+    })
+    context, pozycje_c, tabelarys = faktura_context_calc(context)
+    context_to_pdf(context, pozycje_c, tabelarys, 'test_pozycje', 'faktura_testy')
+    assert os.path.exists('faktura_testy/test_pozycje.pdf')
+def test_nazwa_wrap():
+    context = basic_con()
+    temp = []
+    tempn =''
+    for x in range(100):
+        tempn = 'poz' + str(x) + ' a'*4*x
+        temp += [pozycja(tempn, x)]
+    context.update({
+        'POZYCJE': temp
+    })
+    context, pozycje_c, tabelarys = faktura_context_calc(context)
+    context_to_pdf(context, pozycje_c, tabelarys, 'test_nazwa_wrap', 'faktura_testy')
+    assert os.path.exists('faktura_testy/test_nazwa_wrap.pdf')
+def test_podatki_pozycje():
+    context = basic_con()
+    temp = []
+    for x in range(100):
+        temp += [pozycja(f'poz {x}', x, 1, 23)]
+        temp += [pozycja(f'poz {x}', x, 2, 8)]
+        temp += [pozycja(f'poz {x}', x, 3, 0)]
+    context.update({
+        'POZYCJE': temp
+    })
+    context, pozycje_c, tabelarys = faktura_context_calc(context)
+    context_to_pdf(context, pozycje_c, tabelarys, 'test_podatki_pozycje', 'faktura_testy')
+    assert os.path.exists('faktura_testy/test_podatki_pozycje.pdf')
