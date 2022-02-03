b7b44cafe0f1d1a0d94ff56a46635c13e8704120
Kacper W <poziomy824@gmail.com>
2022-02-03 16:07:27 +0100

    funkcja generowania faktury z panelu admina

--- svg2pdf/admin.py
+++ svg2pdf/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import firma, faktura, pozycjafaktury
 
 # Register your models here.
 
 admin.site.register(firma)
-admin.site.register(faktura)
 admin.site.register(pozycjafaktury)
+
+@admin.register(faktura)
+class fakturaAdmin(admin.ModelAdmin):
+    list_display = ['nazwa','wygeneruj_fakture']
+
+    def nazwa(self, obj):
+        return "Faktura-" + obj.Nazwa_faktury
+    def wygeneruj_fakture(self, obj):
+        return format_html("<a href='/fakturag-{url}/'>Wygeneruj</a>", url=obj.id)
--- svg2pdf/urls.py
+++ svg2pdf/urls.py
@@ -1,3 +1,4 @@
+from re import I
 from django.urls import path
 from . import views
 
