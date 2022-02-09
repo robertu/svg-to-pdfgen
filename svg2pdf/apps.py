f1e2b6386bb358f7847f6da532429712792eb01b
Kacper W <poziomy824@gmail.com>
2022-02-09 12:15:22 +0100

    pre-commit all files

--- svg2pdf/apps.py
+++ svg2pdf/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class Svg2PdfConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'svg2pdf'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "svg2pdf"
