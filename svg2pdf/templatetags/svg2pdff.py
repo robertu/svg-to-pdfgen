4d6a041bc53825198ff327847d0eb98084f086ec
Kacper W <poziomy824@gmail.com>
2022-02-09 11:58:28 +0100

    template tags

--- svg2pdf/templatetags/svg2pdff.py
+++ svg2pdf/templatetags/svg2pdff.py
@@ -2,17 +2,21 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def hight(value):
     return float(value * 0.7)
 
+
 @register.filter
 def floatf(value):
-    return '%.2f' % float(value)
+    return "%.2f" % float(value)
+
 
 @register.filter
 def opistabeli(value):
     return (value + 12) * -1
+
 
 @register.filter
 def kwadrat(value):
