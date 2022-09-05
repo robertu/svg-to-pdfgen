22c676b3c81099feeec51b35bece3f0974e82657
Kacper W <poziomy824@gmail.com>
2022-09-05 12:28:22 +0200

    clean up

--- svg2pdf/urls.py
+++ svg2pdf/urls.py
@@ -1,5 +1,12 @@
+# pylint: disable=missing-module-docstring
 from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.strona_gl), path("faktura-<int:id>/", views.faktura_temp), path("fakturag-<int:id>/", views.faktura_gen)]
+urlpatterns = [
+    path("", views.strona_gl),
+    path("faktura-<int:id>/",
+    views.faktura_temp),
+    path("fakturag-<int:id>/",
+    views.faktura_gen)
+]
